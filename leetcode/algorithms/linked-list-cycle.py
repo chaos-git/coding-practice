4b75f85e51464af61b96a82b2872b9f4cc4eebb9
Kias <kiash@amazon.com>
2018-08-11 13:45:17 -0700

    Adding linked-list-cycle problem - success

---
+++ leetcode/algorithms/linked-list-cycle.py
@@ -0,0 +1,31 @@
+'''
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        slow = head
+        fast = head
+        skip = True
+        while slow and fast:
+            fast = fast.next
+            if not skip:
+                slow = slow.next
+            skip = not skip
+            if slow == fast:
+                return True
+        return False
