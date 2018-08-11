03d316172a88f480483fac4c1b4e4d724ea0baeb
Kias <kiash@amazon.com>
2018-08-11 10:08:40 -0700

    Adding remove-nth-node-from-end-of-list problem - success

---
+++ leetcode/algorithms/remove-nth-node-from-end-of-list.py
@@ -0,0 +1,43 @@
+'''
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        right = head
+        left = None
+        distance = 0
+
+        while right:
+            if distance >= n:
+                left = left.next if left else head
+            right = right.next
+            distance += 1
+
+        if distance >= n and not left:
+            return head.next
+
+        if left:
+            left.next = left.next.next
+
+        return head
